1c2ae3cfb3a729b27e9fe7e7c1c480ab43fb7a6c
ArchUsr64 <anshulusr@gmail.com>
2023-07-24 09:00:21 +0530

    implemented basic symbollic parsing
    Todo: Testing

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,46 @@ enum Symbols {
     Constant(f32),
 }
 
+#[derive(Clone, Copy, Debug)]
+enum ParseError {
+    InvalidConst,
+    UnknownSymbol,
+}
+
 impl Symbols {
-    pub fn from_str(expression: &str) -> Result<Vec<Symbols>, usize> {
-        Err(0)
+    pub fn from_str(expression: &str) -> Result<Vec<Symbols>, (ParseError, usize)> {
+        let mut result = Vec::new();
+        let mut constant_buffer = String::new();
+        for (i, char) in expression.char_indices() {
+            if char.is_ascii_digit() || char == '.' {
+                constant_buffer.push(char);
+                continue;
+            }
+            if !constant_buffer.is_empty() {
+                let parsed_const = constant_buffer
+                    .parse()
+                    .map_err(|_| (ParseError::InvalidConst, i))?;
+                constant_buffer.clear();
+                result.push(Symbols::Constant(parsed_const));
+            }
+            result.push(match char {
+                '+' => Symbols::Plus,
+                '^' => Symbols::Exponent,
+                '*' => Symbols::Multiply,
+                '-' => Symbols::Minus,
+                '/' => Symbols::Divide,
+                '(' => Symbols::OpeningBrace,
+                ')' => Symbols::ClosingBrace,
+                _ => Err((ParseError::UnknownSymbol, i))?,
+            });
+        }
+        if !constant_buffer.is_empty() {
+            let parsed_const = constant_buffer
+                .parse()
+                .map_err(|_| (ParseError::InvalidConst, expression.len() - 1))?;
+            result.push(Symbols::Constant(parsed_const));
+        }
+        Ok(result)
     }
 }
 
@@ -23,10 +60,10 @@ mod tests {
 
     #[test]
     fn evaluate() {
-        let expression = "2+3";
+        let expression = "2-3";
         let symbols_parsed = Symbols::from_str(expression).unwrap();
-        assert_eq!(symbols_parsed[0], Symbols::Constant(2.));
-        assert_eq!(symbols_parsed[1], Symbols::Plus);
-        assert_eq!(symbols_parsed[2], Symbols::Constant(3.));
+        assert_eq!(symbols_parsed[0], Symbols::Constant(2f32));
+        assert_eq!(symbols_parsed[1], Symbols::Minus);
+        assert_eq!(symbols_parsed[2], Symbols::Constant(3f32));
     }
 }
